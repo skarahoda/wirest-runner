7527eeece98f009b57383e93a87f04a4fbf1fe69
SertaçKarahoda <skarahoda@sabanciuniv.edu>
2018-02-17 17:25:52 +0300

    job null check

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 var runner = require('./lib/runner');
-var Parse = require('./lib/parseClient');
+var Parse  = require('./lib/parseClient');
+
 
 function runJob(result){
-	var job       = result.job;
-	var timeout   = job.get('timeout') || job.get('user').get('timeout') || process.env.timeout || 60 * 60 * 10000;
-	var algorithm = job.get('algorithm');
-	var command   = algorithm.get('bashCommand') + ' ' + job.get('parameters');
-	return runner(command, timeout).then(function(result){
+	var job = result.job;
+
+	function saveJob(result){
 		var error = result.error;
 		var errorCode;
 		if(error){
 			errorCode = error.killed ? -1 : error.code;
 		}
 		var params = {
-			jobId: job.id,
+			jobId:     job.id,
 			stdOutput: result.stdOutput,
-			stdError: result.stdError,
+			stdError:  result.stdError,
 			errorCode: errorCode
 		};
 		return Parse.Cloud.run('SaveFinishedJob', params);
-	});
+	}
+
+	if(job != null){
+		var timeout   = job.get('timeout') || job.get('user').get('timeout') || process.env.timeout || 60 * 60 * 10000;
+		var algorithm = job.get('algorithm');
+		var command   = algorithm.get('bashCommand') + ' ' + job.get('parameters');
+		return runner(command, timeout).then(saveJob);
+	}
 }
 
 Parse.Cloud.run('GetNextJobToRun', {}).then(runJob);
