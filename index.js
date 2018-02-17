9db4622ef758a0042b5313982bff87048d89ea8b
SertaçKarahoda <skarahoda@sabanciuniv.edu>
2018-02-17 17:22:57 +0300

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var runner = require('./lib/runner');
+var Parse = require('./lib/parseClient');
+
+function runJob(result){
+	var job       = result.job;
+	var timeout   = job.get('timeout') || job.get('user').get('timeout') || process.env.timeout || 60 * 60 * 10000;
+	var algorithm = job.get('algorithm');
+	var command   = algorithm.get('bashCommand') + ' ' + job.get('parameters');
+	return runner(command, timeout).then(function(result){
+		var error = result.error;
+		var errorCode;
+		if(error){
+			errorCode = error.killed ? -1 : error.code;
+		}
+		var params = {
+			jobId: job.id,
+			stdOutput: result.stdOutput,
+			stdError: result.stdError,
+			errorCode: errorCode
+		};
+		return Parse.Cloud.run('SaveFinishedJob', params);
+	});
+}
+
+Parse.Cloud.run('GetNextJobToRun', {}).then(runJob);
